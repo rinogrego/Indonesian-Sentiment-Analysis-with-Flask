e5cdeef260892591dbc425e0b6b1b6fb49d3cd2b
rinogrego <rino.grego@gmail.com>
2022-06-25 15:25:47 +0700

    modify request post structure

--- test_request_api.py
+++ test_request_api.py
@@ -3,7 +3,13 @@ import pprint
 
 url = 'http://localhost:5000/api/sentiment'
 data = {
-    "text": "SARAN AJA KALO BISA DIBENERIN, BIAR LEBIH BAIK LAGI KE DEPANNYA SOALNYA BANYAK ERROR. TERIMA KASIH"
+    "text": [
+        "SARAN AJA KALO BISA DIBENERIN, BIAR LEBIH BAIK LAGI KE DEPANNYA SOALNYA BANYAK ERROR. TERIMA KASIH",
+        "Itu kenapa dah? kok rusak? kecewa banget gw",
+        "Astaga tolong diperbaiki dong, ini gimana nih? saran aja kalo lama2 ntar rusak",
+        "Senang bat gw, terima kasih. alhamdulillah slalu lancar dan baik. Gak pernah rusak",
+        "Nanti saya tambah ratingnya kalo udah betul, tolong ini kenapa ya? kok error? makanya saya turunin dulu"
+    ]
 }
 
 r = requests.post(url, json=data)
